ce6adca1ac8c1c88ec09966fbb4bdab8177ad54d
wafic <wafic.abess@outlook.com>
2024-04-04 18:32:03 +0300

    Data types

--- 01-FUNDAMENTALS-PART-1/script.js
+++ 01-FUNDAMENTALS-PART-1/script.js
@@ -1,5 +1,6 @@
+/* MULTI LINE COMMENT
 let js = 'amazing';
-console.log(40 + 10 + 10);
+//console.log(40 + 10 + 10);
 
 console.log('jonas');
 console.log(23);
@@ -10,7 +11,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
-let Mike_Nikita = 'MN';
+let Mike_Nikita = "MN";
 let $function = 27;
 //don't use name as variable for it is a keyword though its legal to use.
 //don't start with capital letters.(will be used for object oriented programming).
@@ -20,4 +21,33 @@ let PI = 3.1415; //all UPPER for its a constant (convention of writing all in up
 
 let myFirstJob = 'teacher';
 let job2 = 'programmer';//not descriptive example
+
+//Assignment Values and Variables
+
+let country = "Lebanon";
+let continent = "asia";
+let population = "three million";
+console.log(country, continent, population);
+
+// 7 primitive data types Number/String/Boolean/Undefined/Null/Symbol(ES2015)/BigInt(ES2020)
+*/
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 12);
+console.log(typeof "nikita");
+
+javaScriptIsFun = "yes!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year)
 
