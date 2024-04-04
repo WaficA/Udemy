5c2077fb410943a2d44496b4ab8595dcad0b443b
wafic <wafic.abess@outlook.com>
2024-04-04 17:55:37 +0300

    Values and Variables

--- 01-FUNDAMENTALS-PART-1/script.js
+++ 01-FUNDAMENTALS-PART-1/script.js
@@ -1,5 +1,23 @@
 let js = 'amazing';
-if (js === 'amazing') alert('javaScript is fun')
-
-
 console.log(40 + 10 + 10);
+
+console.log('jonas');
+console.log(23);
+
+let firstName = "Mike";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let Mike_Nikita = 'MN';
+let $function = 27;
+//don't use name as variable for it is a keyword though its legal to use.
+//don't start with capital letters.(will be used for object oriented programming).
+//make sure your variable names are descriptive.
+
+let PI = 3.1415; //all UPPER for its a constant (convention of writing all in upper).
+
+let myFirstJob = 'teacher';
+let job2 = 'programmer';//not descriptive example
+
