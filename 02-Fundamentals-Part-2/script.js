b9d9c7c2ac1d33054650aa0bbf614b1456a79d33
wafic <wafic.abess@outlook.com>
2024-04-11 14:00:13 +0300

    use strict

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let hasDriverLicense = false ;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log('i can drive');
+
+// const interface = 'Audio';    //Examples of reserved
+// const private = 543;
