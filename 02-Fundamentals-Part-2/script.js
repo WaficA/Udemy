a2ceb7ba7692cc54dd985c9e2eaa323e781c6690
wafic <wafic.abess@outlook.com>
2024-04-12 15:14:22 +0300

    Functions, Function Declation, Function Expressions, Arrow Functions, Functions Calling Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // let hasDriverLicense = false ;
 // const passTest = true;
 
@@ -13,7 +13,7 @@ function logger() {
 console.log(`my name is sarah`)
 }
 logger();                    //invoking, running, or calling the function.
-logger();
+// logger();
 
 function fruitProcessor(apples,oranges) {
     // console.log(apples, oranges);
@@ -28,3 +28,49 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
+const num = Number('23');
+
+//function declaration
+// function calAge1 (birthYear){
+// const age = 2037 - birthYear;
+// return age;
+// }
+function calAge1(birthYear){
+    return 2037 - birthYear
+}
+const age1 = calAge1(1991); //Can be called before defined.
+
+//Function expressions
+const calAge2 = function (birthYear){
+    return 2037 - birthYear
+}
+const age2 = calAge2(1991);
+
+//Arrow Function
+const calAge3 = birthYear => 2037 - birthYear;
+const age3 = calAge3(1991);
+console.log(age1, age2, age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 -age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1991, "sarah"));
+*/
+
+//Functions calling another functions
+function cutFruitPieces(fruit) {
+    return fruit * 4
+}
+
+function fruitProcessor(apples,oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
