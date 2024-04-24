4d2f6098f8fd347fecd090a7e82b02d9971ccaec
wafic <wafic.abess@outlook.com>
2024-04-24 17:21:25 +0300

    implementing high score

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -18,6 +18,7 @@ GOOD LUCK 😀
 */
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -30,6 +31,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High';
